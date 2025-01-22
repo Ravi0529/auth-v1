ebc8b8cea780816b7f004f23902e81443168c7d5
Ravi0529 <150603739+Ravi0529@users.noreply.github.com>
2025-01-23 03:01:41 +0530

    user schema added and db connected

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 dotenv.config({
     path: "./.env"
@@ -10,5 +13,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    try {
+        prisma.$connect();
+        console.log(`Server is running on port ${PORT}`);
+        console.log("Database connected successfully");
+    } catch (error) {
+        console.log("Internal Server Error", error);
+    }
 });
