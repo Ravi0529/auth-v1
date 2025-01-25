094fea868bb19bad90a098a1c09aec782bc1ea3f
Ravi0529 <150603739+Ravi0529@users.noreply.github.com>
2025-01-25 14:49:24 +0530

    react-router init for auth

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";
+import Login from "./pages/Login";
+
 const App: React.FC = () => {
   return (
     <>
-      <h1 className="bg-red-500 text-4xl">App</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
