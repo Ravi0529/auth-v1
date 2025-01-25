6b4f95ca7f2d3bf3f15b60d13908721bd206aa93
Ravi0529 <150603739+Ravi0529@users.noreply.github.com>
2025-01-25 21:42:25 +0530

    getHome connected using tanstack

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,36 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
 import Home from "./pages/Home";
 import SignUp from "./pages/SignUp";
 import Login from "./pages/Login";
 
 const App: React.FC = () => {
+
+  const { data: authUser } = useQuery({
+    queryKey: ["authUser"],
+    queryFn: async () => {
+      try {
+        const response = await fetch("/v1/auth/getHome");
+        const data = await response.json();
+        if (data.error) return null;
+        if (!response.ok) {
+          throw new Error(data.error || "Something went wrong");
+        }
+        return data;
+      } catch (error) {
+        throw new Error(error as string);
+      }
+    },
+    retry: false,
+  })
+
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/login" element={<Login />} />
+          <Route path="/" element={authUser ? <Home /> : <Navigate to="/login" />} />
+          <Route path="/signup" element={!authUser ? <SignUp /> : <Navigate to="/" />} />
+          <Route path="/login" element={!authUser ? <Login /> : <Navigate to="/" />} />
         </Routes>
       </BrowserRouter>
     </>
