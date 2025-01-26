97b7c59af1aa9fc19043b8e0f5450f15dde62d5e
Ravi0529 <150603739+Ravi0529@users.noreply.github.com>
2025-01-26 23:20:36 +0530

    bug fix for deployment

--- server/src/server.ts
+++ server/src/server.ts
@@ -25,7 +25,7 @@ app.use("/v1/auth", authRoute);
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "/client/dist")));
 
-    app.get("*", (req, res) => {
+    app.get("*", (_, res) => {
         res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
     });
 }
